1b591b07480f75bf4fe76745e04b5b65b2ab760e
Daanooo <daankraaijeveld@icloud.com>
2024-03-26 16:29:51 +0100

    Create config file when not exist instead of throwing error

--- lib/file_processor.ex
+++ lib/file_processor.ex
@@ -1,10 +1,13 @@
 defmodule ConfigReader.FileProcessor do
   def read_file() do
-    if !File.exists?(".config"), do: {:error, ".config not found"}
+    if !File.exists?(".config"), do: File.touch(".config")
 
     File.stream!(".config")
     |> Enum.map(&String.split(&1, "="))
-    |> Enum.map(fn [key, value] -> {key, String.trim(value)} end)
+    |> Enum.map(fn
+      [key, value] -> {key, String.trim(value)}
+      [key] -> {String.trim(key), ""}
+    end)
     |> Enum.into(%{})
   end
 end
--- test/config_reader_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule ConfigReaderTest do
-  use ExUnit.Case
-
-  doctest ConfigReader
-
-  test "greets the world" do
-    assert ConfigReader.hello() == :world
-  end
-end
